6d0e80d7fafcbe2b1b6215821d0043f49552f040
Anton <amvestergaard2002@gmail.com>
2024-10-25 10:45:01 +0200

    added migrations

---
+++ api/Migrations/20241025083712_finalfields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class finalfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DataCategories_CategoryList",
+                table: "DataProcessor30ListingDatas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataSecurity_SecurityMeasures",
+                table: "DataProcessor30ListingDatas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataTransfer_TransferInformation",
+                table: "DataProcessor30ListingDatas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCategories_CategoryList",
+                table: "DataProcessor30ListingDatas");
+
+            migrationBuilder.DropColumn(
+                name: "DataSecurity_SecurityMeasures",
+                table: "DataProcessor30ListingDatas");
+
+            migrationBuilder.DropColumn(
+                name: "DataTransfer_TransferInformation",
+                table: "DataProcessor30ListingDatas");
+        }
+    }
+}
