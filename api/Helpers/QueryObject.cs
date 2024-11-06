58d473e4cbebb333c709e2465b7d68eebc9a1d80
Anton <amve23@student.aau.dk>
2024-11-06 11:38:00 +0100

    FEAT: Pagination

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -14,5 +14,8 @@ namespace api.Helpers
 
         public bool IsDescending { get; set; } = false;
 
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 32;
     }
 }
