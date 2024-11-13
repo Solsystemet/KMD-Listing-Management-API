94f4579834479b53342986e83c809cde9dc3122b
MeisterRepsak <kasperschnejder@hotmail.com>
2024-11-13 12:37:27 +0100

    Added file scraper to API

--- api/Controllers/FileScraperController.cs
+++ api/Controllers/FileScraperController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.PageSegmenter;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.ReadingOrderDetector;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+using UglyToad.PdfPig.Fonts.Standard14Fonts;
+using UglyToad.PdfPig.Writer;
+using api.FileScraper;
 
 namespace api.Controllers
 {
-    [Route ("api/file-scraper")]
+
+    [Route("api/file-scraper")]
     [ApiController]
     public class FileScraperController : ControllerBase
     {
-        
+       
+
         [HttpPost]
         public IActionResult ScrapeFile(IFormFile file)
         {
@@ -23,8 +33,36 @@ namespace api.Controllers
                 return BadRequest("File must be a PDF");
             }
 
+
+            PdfDocument pdfDocument = FileScraper.FileScraper.OpenPDF(file.OpenReadStream());
+            //Extract ContactInfo
+            (string, List<int>) representatives = FileScraper.FileScraper.GetSection(pdfDocument, "14. Ikrafttræden og ophør", "15. Kontaktpersoner hos den dataansvarlige og databehandleren");
+            (FileScraper.FileScraper.DataController, FileScraper.FileScraper.DataProcessor) representativePair = FileScraper.FileScraper.ExtractDataProcessors(pdfDocument, representatives.Item2);
+
+            // Extract Subprocessors
+            (string, List<int>) SubProcessorSection = FileScraper.FileScraper.GetSection(pdfDocument, "B.1. Godkendte underdatabehandlere", "B.2.");
+            // row major
+            (Dictionary<(double, int), int>, Dictionary<double, int>) tableIndices = FileScraper.FileScraper.CreateTableIndices(pdfDocument, SubProcessorSection.Item2);
+            string[,] SubProcessorData = FileScraper.FileScraper.ExtractSubProcessors(pdfDocument, SubProcessorSection.Item2, tableIndices);
+            var list = new List<List<string>>();
+            for (int i = 0; i < SubProcessorData.GetLength(0); i++)
+            {
+                var innerList = new List<string>();
+                for (int j = 0; j < SubProcessorData.GetLength(1); j++)
+                {
+                    innerList.Add(SubProcessorData[i, j]);
+                }
+                list.Add(innerList);
+            }
+            IEnumerable<FileScraper.FileScraper.SubProcessor>? verifiedSubProcessors = FileScraper.FileScraper.CreateSubProcessorList(list);
+
+            List<object?> objList = new List<object?>();
+            objList.Add(representativePair.Item1);
+            objList.Add(representativePair.Item2);
+            objList.Add(verifiedSubProcessors);
+
+            return Ok(objList);
             
-            return Ok();
         }
     }
 }
---
+++ api/models/DataSubProcessor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    [Owned]
+    public class DataSubProcessor
+    {
+        public string name { get; set; } = String.Empty;
+        public string CVR { get; set; } = String.Empty;
+        public string Adress { get; set; } = String.Empty;
+        public string Treatment { get; set; } = String.Empty;
+        public bool directSubProcessor { get; set; } = false;
+        public string transferReason { get; set; } = String.Empty;
+    }
+}
