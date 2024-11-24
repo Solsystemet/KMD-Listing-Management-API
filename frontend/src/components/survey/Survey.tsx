a43e488b8335cd6ebf0c10a2891e1f0a8670908b
AugustHagedal <144026430+AugustHagedal@users.noreply.github.com>
2024-11-24 18:59:46 +0100

    Changed survey and added  add container function

--- frontend/src/components/survey/Survey.tsx
+++ frontend/src/components/survey/Survey.tsx
@@ -1,85 +1,139 @@
+import { useState } from "react"; // Importing useState to handle state management
 import InputBox from "../inputBox/inputBox";
 import { MultipleSelect } from "../multipleSelect/MultipleSelect";
 import styles from "./Survey.module.css";
 
 function ContactInfo({ index }: { index: number }) {
-   return (
-      <form className={styles.inputContainer}>
-         <p>Contact Info {index}</p>
-         <InputBox templateText={"Name"} />
-         <InputBox templateText={"Position"} />
-         <InputBox templateText={"Telephone number"} />
-         <InputBox templateText={"E-mail"} />
-      </form>
-   );
+  return (
+    <form className={styles.inputContainer}>
+      <p>Contact Info {index}</p>
+      <InputBox templateText={"Name"} />
+      <InputBox templateText={"Position"} />
+      <InputBox templateText={"Telephone number"} />
+      <InputBox templateText={"E-mail"} />
+    </form>
+  );
 }
 
 function DataTransfers({ index }: { index: number }) {
-   return (
-      <form className={styles.inputContainer}>
-         <p>Contact Info {index}</p>
-         <InputBox templateText={"Company name"} />
-         <InputBox templateText={"Address"} />
-         <InputBox templateText={"Treatment of data"} />
-         <InputBox templateText={"Transfer basis"} />
-         <MultipleSelect>{"Is it KMDs sub data-processor?"}</MultipleSelect>
-      </form>
-   );
+  return (
+    <form className={styles.inputContainer}>
+      <p>Data Transfer Info {index}</p>
+      <InputBox templateText={"Company name"} />
+      <InputBox templateText={"Address"} />
+      <InputBox templateText={"Treatment of data"} />
+      <InputBox templateText={"Transfer basis"} />
+      <MultipleSelect>{`Is it KMD's sub data-processor?`}</MultipleSelect>
+    </form>
+  );
+}
+
+function DataProcessors({ index }: { index: number }) {
+  return (
+    <form className={styles.inputContainer}>
+      <p>Data Processor Info {index}</p>
+      <InputBox templateText={"Company name"} />
+      <InputBox templateText={"Address"} />
+      <InputBox templateText={"Treatment of data"} />
+      <InputBox templateText={"Transfer basis"} />
+      <MultipleSelect>{`Is it KMD's sub data-processor?`}</MultipleSelect>
+    </form>
+  );
 }
 
 function TextBox() {
-   return (
-      <textarea
-         name="description"
-         rows={20}
-         className={styles.textBox}
-      ></textarea>
-   );
+  return (
+    <textarea
+      name="description"
+      rows={20}
+      className={styles.textBox}
+    ></textarea>
+  );
 }
 
 type SurveyProps = {
-   numberOfContact: number;
-   numberOfDataTransfers: number;
+  numberOfContact: number;
+  numberOfDataTransfers: number;
+  numberofDataProcessors: number;
 };
 
 export function Survey({
-   numberOfContact,
-   numberOfDataTransfers,
+  numberOfContact,
+  numberOfDataTransfers,
+  numberofDataProcessors,
 }: SurveyProps) {
-   return (
-      <div className={styles.surveyContainer}>
-         <div className={styles.ContactInfoCont}>
-            <h3>Contact info of controller</h3>
+  const [contactInfoCount, setContactInfoCount] = useState<number[]>(
+    new Array(numberOfContact).fill(1)
+  );
+  const [dataTransfersCount, setDataTransfersCount] = useState<number[]>(
+    new Array(numberOfDataTransfers).fill(1)
+  );
+  const [dataProcessorsCount, setDataProcessorsCount] = useState<number[]>(
+    new Array(numberofDataProcessors).fill(1)
+  );
 
-            {Array.from({ length: numberOfContact }).map((_, index) => (
-               <ContactInfo index={index + 1} />
-            ))}
-         </div>
-         <div className={styles.dataCategories}>
-            <h3>Data categories</h3>
-            <MultipleSelect>
-               {"School administrators"}
-               {"School management"}
-               {"Teachers"}
-               {"Other employees"}
-               {"Students"}
-               {"Parents to students under 18 years old"}
-            </MultipleSelect>
-         </div>
-         <div className={styles.dataTransfersContainer}>
-            <h3>Data transfer info</h3>
+  const addContactInfo = () => {
+    setContactInfoCount((prev) => [...prev, prev.length + 1]);
+  };
 
-            {Array.from({ length: numberOfDataTransfers }).map((_, index) => (
-               <DataTransfers index={index + 1} />
-            ))}
-         </div>
-         <div>
-            <h3>
-               Beskrivelse af tekniske og organisatoriske
-               sikkerhedsforanstaltninger.
-            </h3>
-            <TextBox></TextBox>
-         </div>
+  const addDataTransfers = () => {
+    setDataTransfersCount((prev) => [...prev, prev.length + 1]);
+  };
+
+  const addDataProcessors = () => {
+    setDataProcessorsCount((prev) => [...prev, prev.length + 1]);
+  };
+
+  return (
+    <div className={styles.surveyContainer}>
+      <div className={styles.ContactInfoCont}>
+        <h3>Contact info of Data Controller</h3>
+
+        {contactInfoCount.map((_, index) => (
+          <ContactInfo key={index} index={index + 1} />
+        ))}
+        <button onClick={addContactInfo} className={styles.addButton}>
+          Add Contact Info
+        </button>
       </div>
-   );
+
+      <div className={styles.ContactInfoCont}>
+        <h3>Contact info of Data Processor</h3>
+
+        {dataProcessorsCount.map((_, index) => (
+          <DataProcessors key={index} index={index + 1} />
+        ))}
+        <button onClick={addDataProcessors} className={styles.addButton}>
+          Add Data Processor
+        </button>
+      </div>
+
+      <div className={styles.dataCategories}>
+        <h3>Data categories</h3>
+        <MultipleSelect>
+          {"School administrators"}
+          {"School management"}
+          {"Teachers"}
+          {"Other employees"}
+          {"Students"}
+          {"Parents to students under 18 years old"}
+        </MultipleSelect>
+      </div>
+      <div className={styles.dataTransfersContainer}>
+        <h3>Data transfer info</h3>
+        {dataTransfersCount.map((_, index) => (
+          <DataTransfers key={index} index={index + 1} />
+        ))}
+        <button onClick={addDataTransfers} className={styles.addButton}>
+          Add Data Transfer
+        </button>
+      </div>
+      <div>
+        <h3>
+          Beskrivelse af tekniske og organisatoriske sikkerhedsforanstaltninger.
+        </h3>
+        <TextBox></TextBox>
+      </div>
+    </div>
+  );
 }
--- frontend/src/routes/create-listing/index.lazy.tsx
+++ frontend/src/routes/create-listing/index.lazy.tsx
@@ -1,17 +1,21 @@
-import styles from './createListing.module.css'
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { Survey } from '../../components/Survey/Survey'
+import styles from "./createListing.module.css";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { Survey } from "../../components/Survey/Survey";
 
-export const Route = createLazyFileRoute('/create-listing/')({
+export const Route = createLazyFileRoute("/create-listing/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
     <main className={styles.createListing}>
       <div>
-        <Survey numberOfContact={3} numberOfDataTransfers={3}></Survey>
+        <Survey
+          numberOfContact={1}
+          numberofDataProcessors={1}
+          numberOfDataTransfers={3}
+        ></Survey>
       </div>
     </main>
-  )
+  );
 }
