a62e06405640c45fb53d853e561bdf65f7680315
Jacob Mølholt Poulsen <43791907+lindejacob@users.noreply.github.com>
2024-12-05 12:59:47 +0100

    Form done, now its just security + post data

--- frontend/src/components/survey/Survey.tsx
+++ frontend/src/components/survey/Survey.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"; // Importing useState to handle state management
 import InputBox from "../inputBox/inputBox";
 import { useForm, Field } from "@tanstack/react-form";
-import { Checkbox, MultipleSelect } from "../checkBox/CheckBox";
+import { Checkbox } from "../checkBox/CheckBox";
 import styles from "./Survey.module.css";
 import NullableDataProcessor30ListingData from "../../types/NullableDataProcessor30ListingData";
 
@@ -23,6 +23,7 @@ function TextBox({ value }: { value?: string }) {
       </textarea>
    );
 }
+
 type SurveyProps = {
    listingDataProp: NullableDataProcessor30ListingData;
 };
@@ -64,10 +65,11 @@ export function Survey({ listingDataProp }: SurveyProps) {
 
    return (
       <div className={styles.surveyContainer}>
+         {/* Listing information */}
          <div className={styles.ContactInfoCont}>
             <Field form={form} name="listingName">
                {field => (
-                  <>
+                  <div className={styles.inputContainer}>
                      <h1>Data Processor 30 Listing information</h1>
                      <InputBox
                         templateText="Enter listing name"
@@ -78,10 +80,142 @@ export function Survey({ listingDataProp }: SurveyProps) {
                         onChange={e => field.handleChange(e.target.value)}
                      />
                      <FieldInfo field={field} />
-                  </>
+                  </div>
+               )}
+               <div />
+            </Field>
+
+            {/* Data controller representative */}
+            <Field form={form} name="dataControllerRepresentative">
+               {field => (
+                  <div className={styles.inputContainer}>
+                     <h2>Data Controller Representative info</h2>
+                     <InputBox
+                        id={`dataControllerRepresentative.name`}
+                        name={`dataControllerRepresentative.name`}
+                        templateText={"Name"}
+                        value={
+                           listingData.dataControllerRepresentative.name || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataControllerRepresentative.role`}
+                        name={`dataControllerRepresentative.role`}
+                        templateText={"Role"}
+                        value={
+                           listingData.dataControllerRepresentative.role || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataControllerRepresentative.address`}
+                        name={`dataControllerRepresentative.address`}
+                        templateText={"Address"}
+                        value={
+                           listingData.dataControllerRepresentative.address ||
+                           ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataControllerRepresentative.phoneNo`}
+                        name={`dataControllerRepresentative.phoneNo`}
+                        templateText={"Phone number"}
+                        value={
+                           listingData.dataControllerRepresentative.phoneNo ||
+                           ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataControllerRepresentative.mail`}
+                        name={`dataControllerRepresentative.mail`}
+                        templateText={"E-mail"}
+                        value={
+                           listingData.dataControllerRepresentative.mail || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                  </div>
                )}
             </Field>
-            <Field form={form} name="DataControllerRepresentative"></Field>
+
+            {/* Data processor representative */}
+            <Field form={form} name="dataProcessorRepresentative">
+               {field => (
+                  <div className={styles.inputContainer}>
+                     <h2>Data Processor Representative info</h2>
+                     <InputBox
+                        id={`dataProcessorRepresentative.name`}
+                        name={`dataProcessorRepresentative.name`}
+                        templateText={"Name"}
+                        value={
+                           listingData.dataProcessorRepresentative.name || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataProcessorRepresentative.role`}
+                        name={`dataProcessorRepresentative.role`}
+                        templateText={"Role"}
+                        value={
+                           listingData.dataProcessorRepresentative.role || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataProcessorRepresentative.address`}
+                        name={`dataProcessorRepresentative.address`}
+                        templateText={"Address"}
+                        value={
+                           listingData.dataProcessorRepresentative.address || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataProcessorRepresentative.phoneNo`}
+                        name={`dataProcessorRepresentative.phoneNo`}
+                        templateText={"Phone number"}
+                        value={
+                           listingData.dataProcessorRepresentative.phoneNo || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                     <InputBox
+                        id={`dataProcessorRepresentative.mail`}
+                        name={`dataProcessorRepresentative.mail`}
+                        templateText={"E-mail"}
+                        value={
+                           listingData.dataProcessorRepresentative.mail || ""
+                        }
+                        onBlur={field.handleBlur}
+                        onChange={field.handleChange}
+                     />
+                     <FieldInfo field={field} />
+                  </div>
+               )}
+            </Field>
+
+            {/* Data controller */}
             <Field form={form} name="dataController">
                {field => (
                   <div className={styles.inputContainer}>
@@ -134,8 +268,8 @@ export function Survey({ listingDataProp }: SurveyProps) {
                   </div>
                )}
             </Field>
-            {/* Sub proccesors */}
-            /* ! Add import of data from scraper */
+
+            {/* Sub processors */}
             <h2>Data Sub Processors</h2>
             {listingData.dataSubProcessors.map((subProcessor, index) => (
                <div key={index}>
